e4f58f27b8c6b9a9e113ad049f4ec35cd51ced99
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-06-25 03:20:50 +0900

    week5_clonecoding
    clone coding - 완료
    5주차 과제 - fragment를 이용한 내비게이션 전환 O, 데이터 전달 X (진행중)

---
+++ week5_fragmentStudy/app/src/main/java/com/example/week5_fragmentstudy/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.week5_fragmentstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.week5_fragmentstudy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.framelayout,HomeFragment()).commit()
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home->{
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout,HomeFragment()).commit()
+                }
+                R.id.search->{
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout,SearchFragment()).commit()
+                }
+                R.id.ranking->{
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout,RankingFragment()).commit()
+                }
+                R.id.mypage->{
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout,MypageFragment()).commit()
+                }
+            }
+            true
+        }
+    }
+}
---
+++ week5_fragmentStudy/app/src/main/java/com/example/week5_fragmentstudy/MypageFragment.kt
@@ -0,0 +1,32 @@
+package com.example.week5_fragmentstudy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.week5_fragmentstudy.databinding.FragmentHomeBinding
+import com.example.week5_fragmentstudy.databinding.FragmentMypageBinding
+
+class MypageFragment : Fragment() {
+    private var _binding: FragmentMypageBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMypageBinding.inflate(inflater,container,false)
+        var view = binding.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
