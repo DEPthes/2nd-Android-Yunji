c8161819982b75546cac5084d4413ba35c420a18
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-11-21 20:04:37 +0900

    [mod] viewModel 코드 수정

--- IndepthStudy_week02/app/src/main/java/com/example/indepthstudy_week02/MainActivity.kt
+++ IndepthStudy_week02/app/src/main/java/com/example/indepthstudy_week02/MainActivity.kt
@@ -3,26 +3,29 @@ package com.example.indepthstudy_week02
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.indepthstudy_week02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: UserViewModel
+    private var viewModel: UserViewModel?=null
     private lateinit var viewModelFactory: UserViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        viewModelFactory = UserViewModelFactory("","")
-        viewModel = ViewModelProvider(this, viewModelFactory).get(UserViewModel::class.java)
-
+        Log.d("TAG", "onCreate: viewmodel before")
+        if(viewModel==null){
+            Log.d("TAG", "onCreate: viewmodel null")
+            initViewModel()
+        }
+        Log.d("TAG", "onCreate: viewmodel after")
         binding.btnPrint.setOnClickListener {
             val firstName = binding.etFirstName.text.toString()
             val lastName = binding.etLastName.text.toString()
 
-            viewModel.setName(firstName,lastName)
+            viewModel?.setName(firstName,lastName)
             binding.user = viewModel
 //            binding.user = User(firstName,lastName)
         }
@@ -31,5 +34,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun initViewModel(){
+        viewModelFactory = UserViewModelFactory("","")
+        viewModel = ViewModelProvider(this, viewModelFactory).get(UserViewModel::class.java)
     }
 }
