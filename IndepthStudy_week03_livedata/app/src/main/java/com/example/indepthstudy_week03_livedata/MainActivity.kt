55edc98825167bbd085de684e1ba98754cdcd8a9
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-12-18 22:34:43 +0900

    [수정]

--- IndepthStudy_week03_livedata/app/src/main/java/com/example/indepthstudy_week03_livedata/MainActivity.kt
+++ IndepthStudy_week03_livedata/app/src/main/java/com/example/indepthstudy_week03_livedata/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.indepthstudy_week03_livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import com.example.indepthstudy_week03_livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mutableLiveData: MutableLiveData<Int> = MutableLiveData(0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btn.setOnClickListener{
+            mutableLiveData.value = mutableLiveData.value!!.plus(1)
+        }
+
+        mutableLiveData.observe(this) {
+            binding.tv.text = mutableLiveData.value.toString()
+        }
+
     }
 }
